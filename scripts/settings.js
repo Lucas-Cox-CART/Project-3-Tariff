d7945a23dcba3f428766c4d1c2e6b82d6c87d529
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-03-19 18:19:49 -0700

    shenanagens
    changes

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,29 +1,29 @@
 let difficultySliderSettings = false;
-document.getElementById('event1').style.display = 'none';
-document.getElementById('event2').style.display = 'none';
+document.getElementById('E1').style.display = 'none';
+document.getElementById('E2').style.display = 'none';
 
 /* This function causes main page and footer to disappear, and causes Settings 
 menu to appear. Also calls other Settings functions. */
 function SettingsMenu() {
     difficultySliderSettings = true;
-    document.getElementById('event0').style.display = 'none';
-    document.getElementById('event1').style.display = 'inline';
+    document.getElementById('E0').style.display = 'none';
+    document.getElementById('E1').style.display = 'inline';
     document.getElementById('footer').style.display = 'none';
 }
 
-let difficulty = document.getElementById("difficultySelect");  
+let difficulty = document.getElementById("diffSelect");  
 // Handles options for difficultySelect menu popup
 difficulty.addEventListener("change", (e) => {
-    difficulty = document.getElementById('difficultySelect').value;
+    difficulty = document.getElementById('diffSelect').value;
 });
 
-document.getElementById("startingBudgetSelect").value = 600000; 
-let startingBudgetDocument = document.getElementById("startingBudgetSelect");  
+document.getElementById("SBSelect").value = 600000; 
+let startingBudgetDocument = document.getElementById("SBSelect");  
 let startingBudget = 600000;
 
-// Handles options for startingBudgetSelect menu popup
+// Handles options for SBSelect menu popup
 startingBudgetDocument.addEventListener("change", (e) => {
-    startingBudget = document.getElementById("startingBudgetSelect").value;
+    startingBudget = document.getElementById("SBSelect").value;
     startingBudget = Math.floor(startingBudget);
 });
 
@@ -31,45 +31,45 @@ startingBudgetDocument.addEventListener("change", (e) => {
 // Default Settings for Easy, Normal, and Hard Difficulties.
 difficulty.addEventListener("change", (e) => {
         if (difficulty == NaN || difficulty == 1) {
-            document.getElementById("startingBudgetSelect").value = 600000;
+            document.getElementById("SBSelect").value = 600000;
             startingBudget = 600000;
-            document.getElementById("generalTaxSelect").value = 9;
+            document.getElementById("GTSelect").value = 9;
             generalTax = 0.09;
         } else if (difficulty == 2) {
-            document.getElementById("startingBudgetSelect").value = 400000;
+            document.getElementById("SBSelect").value = 400000;
             startingBudget = 400000;
-            document.getElementById("generalTaxSelect").value = 12;
+            document.getElementById("GTSelect").value = 12;
             generalTax = 0.12;
         } else if (difficulty == 3) {
-            document.getElementById("startingBudgetSelect").value = 200000;
+            document.getElementById("SBSelect").value = 200000;
             startingBudget = 200000;
-            document.getElementById("generalTaxSelect").value = 16;
+            document.getElementById("GTSelect").value = 16;
             generalTax = 0.16;
         } else if (difficulty == 4) {
-            document.getElementById("startingBudgetSelect").value = "";
+            document.getElementById("SBSelect").value = "";
             startingBudget = "";
-            document.getElementById("generalTaxSelect").value = "";
+            document.getElementById("GTSelect").value = "";
             generalTax = "";
         }
 });
 
 let playerBudget;
-startingBudgetSelect.addEventListener('click', (e) => {
-    document.getElementById('startingBudgetSelect').value = playerBudget;
+SBSelect.addEventListener('click', (e) => {
+    document.getElementById('SBSelect').value = playerBudget;
 });
 
-let shareScreen = document.getElementById("shareScreenSelect"); 
-// Handles options for shareScreenSelect menu popup
+let shareScreen = document.getElementById("SCSelect"); 
+// Handles options for SCSelect menu popup
 shareScreen.addEventListener("change", (e) => {
-    shareScreen = document.getElementById("shareScreenSelect").value;
+    shareScreen = document.getElementById("SCSelect").value;
 });
 
-let generalTax = document.getElementById("generalTaxSelect");  
-// Handles options for startingBudgetSelect menu popup
+let generalTax = document.getElementById("GTSelect");  
+// Handles options for SBSelect menu popup
 generalTax.addEventListener("change", (e) =>generalTaxFunction());
 
 // Default Tax Value
-document.getElementById("generalTaxSelect").value = 9; 
+document.getElementById("GTSelect").value = 9; 
 generalTax = 0.09;
 
 
@@ -79,7 +79,7 @@ let playerAmount = "";
 let players = [];
 
 function playerCreation(a) {
-    playerAmount = playerCountSelect.value;
+    playerAmount = PCSelect.value;
     for (let i = 0; i < playerAmount; i++) {
         players.push(player = {
             budget: a,
