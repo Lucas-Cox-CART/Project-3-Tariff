0b33a28ff9115df1351bbad2156fe3ab3c679dfb
Lucas-Cox-CART <lucas.cox@student.cart.org>
2023-03-19 18:26:37 -0700

    Revert "shenanagens"
    This reverts commit d7945a23dcba3f428766c4d1c2e6b82d6c87d529.

--- scripts/dice+board.js
+++ scripts/dice+board.js
@@ -1,4 +1,4 @@
-let gameBoard = document.getElementById('E2R');
+let gameBoard = document.getElementById('event2sub');
 
 let world = {
     worldEventCooldown: false,
@@ -10,8 +10,8 @@ function startGame() {
     if (shareScreen == 1) {
         alert("You need to select an option for Share Screen.");
     } else {
-        document.getElementById('E1').style.display = 'none';
-        document.getElementById('E2').style.display = 'flex';
+        document.getElementById('event1').style.display = 'none';
+        document.getElementById('event2').style.display = 'flex';
         playerCreation(startingBudget);
     }
     pcLogo();
@@ -24,8 +24,8 @@ for (let i = 0; i < 40; i++) {
 }
 
 
-let DL1 = document.getElementById('DL1');
-let DL2 = document.getElementById('DL2');
+let dL1 = document.getElementById('dL1');
+let dL2 = document.getElementById('dL2');
 let diceValue1;
 let diceValue2;
 let movement;
@@ -37,8 +37,8 @@ function rollDice(x) {
     if (players[x].jailed == true) {
         if (diceValue1 == diceValue2) {
             playerMove(x);
-            DL1.classList.remove(`D${1-6}`);
-            DL2.classList.remove(`D${1-6}`);
+            dL1.classList.remove(`D${1-6}`);
+            dL2.classList.remove(`D${1-6}`);
         }
     } else {
         playerMove(x);
@@ -47,42 +47,42 @@ function rollDice(x) {
     function diceAnim() {
         switch (diceValue1) {
             case 1: 
-                DL1.classList.add('D1');
+                dL1.classList.add('D1');
                 break;
             case 2:
-                DL1.classList.add('D2');
+                dL1.classList.add('D2');
                 break;
             case 3:
-                DL1.classList.add('D3');
+                dL1.classList.add('D3');
                 break;
             case 4:
-                DL1.classList.add('D4');
+                dL1.classList.add('D4');
                 break;
             case 5:
-                DL1.classList.add('D5');
+                dL1.classList.add('D5');
                 break;
             case 6:
-                DL1.classList.add('D6');
+                dL1.classList.add('D6');
                 break;
         }
         switch (diceValue2) {
             case 1: 
-                DL2.classList.add('D1');
+                dL2.classList.add('D1');
                 break;
             case 2:
-                DL2.classList.add('D2');
+                dL2.classList.add('D2');
                 break;
             case 3:
-                DL2.classList.add('D3');
+                dL2.classList.add('D3');
                 break;
             case 4:
-                DL2.classList.add('D4');
+                dL2.classList.add('D4');
                 break;
             case 5:
-                DL2.classList.add('D5');
+                dL2.classList.add('D5');
                 break;
             case 6:
-                DL2.classList.add('D6');
+                dL2.classList.add('D6');
                 break;
         }
     }
@@ -92,7 +92,7 @@ function rollDice(x) {
 let playerIcon = document.createElement('span');
 function createPlayers() {
     playerIcon.classList.add('active', 'frog');
-    cell[1].appendChild(playerIcon);
+    cell[30].appendChild(playerIcon);
     players[turnCycle].playerPosition = 30;
 }
 
@@ -153,11 +153,14 @@ function goCheck(x) {
         for (let player of players) {
             player.goCounter = 0;
             player.budget = player.budget + ((50000 + (player.property['length'] * ( 50000 * 0.33))) - ((50000 + (player.property['length'] * ( 50000 * 0.33))) * generalTax));
+            // Player's budget = Player's budget - (Player's budget * the current tax)
+            //The "for loop" gives all players annual income, and pushes them all to go. 
+            //50,000$ is the default annual income. 
         }
-        for (let player of players) {
-            player.playerPosition = cell[0];
+        for (let player of players) { 
+            player.playerPosition = cell[30]; //
         }
-        election();
+        election(); 
     } else {
         playerTurnEnd();
     }
@@ -212,6 +215,7 @@ function playerTurnEnd(x) {
     }
     diceValue1 = 0;
     diceValue2 = 0;
+    extrasDetect();
     chanceDetect(turnCycle); //Checks if the player is on a chance tile
     chestDetect(turnCycle); //Checks if the player is on a chest tile
 }
--- scripts/properties.js
+++ scripts/properties.js
@@ -11,19 +11,27 @@ let tiles = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
 function propertyIdentify(x,y) { //This function determines what property the player is on
     if (players[x].playerPosition != 8 || 22 || 36 || 2 || 17 || 33 || 4 || 38 || 0 || 10 || 20 || 30) {
         if (properties[x] = 0) {
-            playProperty()
+            playProperty();
         } else if (properties[x] = 1) {
-            payRent(y)
+            payRent(y);
         }
     }
 }
 
 function playProperty(x) {
     if (properties[players[x].playerPosition] == 0) {
-        players[x].budget = players[x].budget - (propertyData[players[x].playerPostion][10])
+        if (players[x].budget > propertyData[players[x].playerPostion][10]) {
+        players[x].budget = players[x].budget - propertyData[players[x].playerPostion][10];
         properties[players[x].playerPosition] = players[x]
+        } else {
+            //turn end or something
+        }
     } else if (properties[players[x].playerPosition] == players[x]) {
-        buyBuildings()
+        if (players[x].budget > propertyData[players[x].playerPostion][8]) {
+        buyBuildings();
+        } else {
+            //turn end or something
+        }
     } else {
         players[x].budget = players[x].budget - (propertyData[players[x].playerPostion][1])
     }
@@ -31,8 +39,13 @@ function playProperty(x) {
 }
 
 function buyBuildings() { //for buying houses and whatnot
+ //some function to create UI to choose to buy house or not
+    if (players[x].playerposition == 11 || players[x].playerposition == 27) {
+        //turn end
+    } else {
 
-}
+    }
+} 
 
 function chanceDetect(turnCycle) {
     if (players[turnCycle].playerPosition  == 38 || players[turnCycle].playerPosition == 12 || players[turnCycle].playerPosition == 26){ // If player is on chance tile or chest tile
@@ -46,9 +59,27 @@ function chanceDetect(turnCycle) {
 function chestDetect(turnCycle) {
     if (players[turnCycle].playerPosition == 7 || players[turnCycle].playerPosition == 32 || players[turnCycle].playerPosition == 23) {
         performChestCard();
-        console.log("It works")
+        console.log("It works");
     } else {
-        console.log("The code does not recognize the player is on a chest card")
+        console.log("The code does not recognize the player is on a chest card");
+    }
+}
+
+function extrasDetect() {
+    if (players[turnCycle].playerPosition ==  2) {
+        players[turnCycle].budget = players[turnCycle].budget - (players[turnCycle].budget * (generalTax + 0.15));
+    }
+    //If the player lands on the elec. bill, they have to pay generalTax + 0.15.
+    if (players[turnCycle].playerPosition ==  18) {
+        if (players[turnCycle].budget > 37500) {
+            //Display option for if they want to buy water works
+            players[turnCycle].property.push(propertyData[26]);
+        }
+    }
+    //If the player lands on water works, they have the option to buy it. It can not be expanded on like properties. 
+    if (players[turnCycle].playerPosition ==  36) {
+        players[turnCycle].budget = players[turnCycle].budget - (players[turnCycle].budget * (generalTax + 0.15));
+        //Functions the same as the elec.bill
     }
 }
 
